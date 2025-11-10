c8447d963342ab8b6213db4da55e53bfe2485840
Anastasiia <anastasiia0matsiuk@gmail.com>
2025-11-10 20:52:34 +0200

    fix task3

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function filterArray(numbers, value) {
-    newArray = [];
+    const newArray = [];
     for (let i = 0; i < numbers.length; i += 1) {
         if (numbers[i] > value) {
             newArray.push(numbers[i]);
